ce84c0645e300e393d4ffa0a0fcd12e8d52829e7
Leonardo Machado Britto Junior <leo.dev2001@gmail.com>
2024-10-28 13:03:02 +0000

    Commitando 2

--- Funcoes.py
+++ Funcoes.py
@@ -1,9 +1,38 @@
+def menu():
+    print("""
+1- CHECAR ANOTAÇÕES
+2- ADICIONAR ANOTAÇÃO
+3- EXCLUIR ANOTAÇÃO
+""")
+    
 def criando ():
-    from main import Diario, session
+    from main import Diario, session,datetime
+    dia_pesquisa = int(input("DIA: "))
+    mes_pesquisa = int(input("MÊS: "))
+    ano_pesquisa = int(input("ANO: "))
+    data_time = datetime(ano_pesquisa, mes_pesquisa, dia_pesquisa)
     diario = Diario(
-        data = input("DATA: "),
-        titulo = input("TITULO: "),
-        texto = input("")
+        data=data_time,
+        titulo=input("TITULO: "),
+        texto=input("TEXTO: ")
     )
     session.add(diario)
-    session.commit
+    session.commit()
+    print("Anotação adicionada com sucesso!")
+
+def excluir_anotacao():
+    from main import Diario, session,datetime
+    dia_pesquisa = int(input("DIA: "))
+    mes_pesquisa = int(input("MÊS: "))
+    ano_pesquisa = int(input("ANO: "))
+    
+    data_time = datetime(ano_pesquisa, mes_pesquisa, dia_pesquisa)
+    
+    anotacao = session.query(Diario).filter(Diario.data == data_time).first()
+    
+    if anotacao:
+        session.delete(anotacao)
+        session.commit()
+        print("Anotação excluída com sucesso!")
+    else:
+        print("Nenhuma anotação encontrada para essa data.")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from sqlalchemy import create_engine, Integer, String, Column
+from datetime import datetime
+from sqlalchemy import create_engine, Integer, String, Column,DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
 from dataclasses import dataclass
-from Funcoes import criando
+from Funcoes import criando,menu,excluir_anotacao
+
 
 BD = create_engine("sqlite:///notasbd.bd")
 
@@ -13,13 +15,38 @@ session = Session()
 @dataclass
 class Diario(Base):
     __tablename__ = "darios"
-    id = Column(Integer, primary_key=True,autoincrement=True)
-    data = Column(String)
+    data = Column(DateTime,primary_key=True)
     titulo = Column(String)
     texto = Column(String)
 
 Base.metadata.create_all(bind=BD) 
 
 while True:
-    criando()
+    menu()
+    opcao =int(input(": "))
+    if opcao in [1, 2, 3]:
+        match opcao:
+            case 1:
+                dia_pesquisa = int(input("DIA: "))
+                mes_pesquisa = int(input("MÊS: "))
+                ano_pesquisa = int(input("ANO: "))
+                data_pesquisa = datetime(ano_pesquisa, mes_pesquisa, dia_pesquisa)
+                anotacao = session.query(Diario).filter(Diario.data == data_pesquisa).first()
+                print("="*20)
+                if anotacao:
+                    print(f"{anotacao.titulo:^20}")
+                    print("="*20)
+                    print(anotacao.texto)
+                else:
+                    print("Nenhuma anotação encontrada para essa data.")
+            case 2:
+                criando()
+            case 3:
+                excluir_anotacao()
+    else:            
+        print("OPÇÂO INVALIDA")
+
+
+
+
 
